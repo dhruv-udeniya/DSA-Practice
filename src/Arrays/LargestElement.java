ee4b7acee1a46c567725e967634e7420f6fe0d96
dhruv-udeniya <175002254+dhruv-udeniya@users.noreply.github.com>
2026-07-17 13:32:22 +0530

    Second Largest Element in Array

--- src/Arrays/LargestElement.java
+++ src/Arrays/LargestElement.java
@@ -22,12 +22,31 @@ class LargestElement{
         return max;
     }
 
+    public int SecondMaxNum(int[] nums){
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        int smax = -1;
+        for (int i = 0 ; i<n ; i++){
+            max = Math.max(max,nums[i]);
+        }
+        for (int i = 0 ; i<n ; i++){
+            if (nums[i]<max){
+                if (smax<nums[i]){
+                    smax = nums[i];
+                }
+            }
+        }
+        return smax;
+    }
+
     public static void main(String[] args){
         LargestElement l1 = new LargestElement();
-        int nums[] = {1, 6, 2, 10, 3};
+        int nums[] = {1, 6, 2, 10, 3,9};
 //        int res = l1.MaxNum(nums);
+        int sres = l1.SecondMaxNum(nums);
         int res = l1.MaxNo(nums);
-        System.out.println(res);
+        System.out.println("Maximum Element in Array-:"+res);
+        System.out.println("Second Largest Element-:"+sres);
 
     }
 }
